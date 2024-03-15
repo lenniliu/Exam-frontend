848b6efaa76899b2b35847a97cf92ee454d24d60
lenniliu <lennil@metropolia.fi>
2024-03-15 10:57:34 +0200

    Co-authored-by: Julia Tokola <juliatok@users.noreply.github.com>

---
+++ frontend/src/components/paymentForm.js
@@ -0,0 +1,6 @@
+import { useState } from "react";
+
+const paymentForm = () => {
+    const [amount, setAmount] = useState("");
+    const [amount, setAmount] = useState("");
+}
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+      <div className="home">
+        <div className="myservice">
+          Home component
+        </div>
+      </div>
+    );
+  };
+  
+  export default Home;
